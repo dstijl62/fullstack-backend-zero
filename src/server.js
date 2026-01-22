920ffa5fffbea57559b15d8e06f1331ef293bacd
dstijl62 <dstijl62@gmail.com>
2026-01-22 21:07:35 +0700

    bai 21 static config

--- src/server.js
+++ src/server.js
@@ -34,9 +34,13 @@ const hostname = process.env.HOST_NAME;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//config static file: image/css/js
+// app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Khai báo route
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! & Nodemon')
 })
 
 app.get('/abc', (req, res) => {
